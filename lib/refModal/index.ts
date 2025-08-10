319341e4fabf8b723ab905f599c0ac97a84e0d8b
Bardia Razi <bardia.d1375@gmail.com>
2025-08-10 15:04:19 +0330

    add index file to reModal directory

---
+++ lib/refModal/index.ts
@@ -0,0 +1,9 @@
+export { default as RefModal } from "./RefModal";
+
+export type { 
+  DataResolve, 
+  DefaultRefModalProps, 
+  ModalHandle, 
+  ModalPropsBase, 
+  PropsOpenModal 
+} from "./types";
