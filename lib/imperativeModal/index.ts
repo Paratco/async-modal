6a660f8d8f368d4dd9bb57f8153ae1bf7f603d52
Nik <Nik96.ge@gmail.com>
2025-08-12 12:27:13 +0330

    feat: add imperative modal

---
+++ lib/imperativeModal/index.ts
@@ -0,0 +1,3 @@
+export type { ImperativeModalApi } from "./types";
+
+export { ImperativeModal } from "./ImperativeModal";
--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,6 @@ export type { AsyncModalProps } from "./types";
 
 export * from "./containerModal";
 
+export * from "./imperativeModal";
+
 // export * from "./providerModal";
